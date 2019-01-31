ac1787cb473f2c1d8c00c4f1ab7fde1858b91da7
cvonwilczur <cvonwilczur@gmail.com>
2019-01-31 08:09:30 -0700

    notes on bytes, shorts and ints

---
+++ ByteShortInt/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte myByteValue = 10;
+
+        short myShortValue = 24500;
+
+        long myLongValue = 1_248_354_019_034_821L;
+
+        byte myNewBiteValue = (byte) (myBiteValue/2);
+
+    }
+}
