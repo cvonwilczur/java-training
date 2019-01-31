750b08995f1cd532b59f21d01b4ae499af97fdd6
cvonwilczur <cvonwilczur@gmail.com>
2019-01-31 08:16:29 -0700

    float and double notes

---
+++ FloatandDouble/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myIntValue = 5/2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+        
+    }
+}
